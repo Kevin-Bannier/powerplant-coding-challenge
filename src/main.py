b502de570029d87ca87dc6f7102ee5463464e0b6
Kevin Bannier <kevinbannier1@gmail.com>
2024-02-08 14:37:52 +0100

    feature: add logger

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-
 from waitress import serve
-
-
 from server import create_server
+
 
 def main():
     srv = create_server()
-    serve(srv, host='0.0.0.0', port=8080)
+    serve(srv, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
--- src/server.py
+++ src/server.py
@@ -1,4 +1,14 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
+
+import logging
+
+# TODO(kba): move logger to config file
+logger = logging.getLogger()
+logger.setLevel("INFO")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 
@@ -15,5 +25,13 @@ def create_server(test_config=None) -> Flask:
     @app.post('/productionplan')
     def productionplan() -> tuple[Response, int]:
         return jsonify({"key": "value"}), 200
+
+
+    # TODO(kba): catch errors
+    # Logs
+    @app.after_request
+    def after_request(response:Response) -> Response:
+        logger.info("%s %s %s", request.method, request.path, response.status)
+        return response
 
     return app
